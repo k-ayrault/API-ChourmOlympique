38b01ba2de2a3392963b90ceff67f0379f8db7bc
k-ayrault <63095040+k-ayrault@users.noreply.github.com>
2022-04-26 08:08:12 +0200

    Correction script création joueur journalié

--- CreationJoueurDuJour.php
+++ CreationJoueurDuJour.php
@@ -1,3 +1,15 @@
 <?php
+// Création d'une nouvelle ressource cURL
+$ch = curl_init();
 
-header('location:https://api.chourmolympique.fr/api/joueur_du_jour/creer');
+// Configuration de l'URL et d'autres options
+curl_setopt($ch, CURLOPT_URL, "http://api.chourmolympique.fr/api/joueur_du_jour/creer");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// Récupération de l'URL et affichage sur le navigateur
+if( ! $result = curl_exec($ch))
+{
+    trigger_error(curl_error($ch));
+}
+// Fermeture de la session cURL
+curl_close($ch);
